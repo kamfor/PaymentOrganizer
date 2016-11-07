8a74376fd306aec31ec0502c497d4889f6ab0c58
kamfor <kfforex@gmail.com>
2016-11-07 23:25:04 +0100

    started classyfing

---
+++ class/Payment.java
@@ -0,0 +1,20 @@
+public class Payment{
+	int date; 
+	int end_date; 
+	float value; 
+	string document_number; 
+	string notes; 
+	bool active
+	int actual_payoff; 
+	
+
+	void returnDate() {
+	   
+	}
+   
+	void pay(int arg_date, float arg_value){
+	   
+	}
+
+
+}
